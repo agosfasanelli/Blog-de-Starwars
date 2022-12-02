721390410191adfbc949b667b0f6f1670c2a3cc2
agosfasanelli <agosfasanelli@gmail.com>
2022-12-02 11:58:20 +0000

    hcsabdjsa

--- src/js/views/character.js
+++ src/js/views/character.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useContext } from "react";
+import { Context } from "../../store/appContext";
 import { Link } from "react-router-dom"
 
 const Character = (props) => {
 
+    const { store, actions } = useContext(Context);
     
     return (
         <div className="row">
